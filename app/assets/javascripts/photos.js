fb653747b89cc24e98c962a038aae3310a537f5c
Duy Lai <duyklai@gmail.com>
2019-11-05 23:44:02 -0600

    Add controller characters, basic image, test js on image

---
+++ app/assets/javascripts/photos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Handler when the DOM is fully loaded
+  console.log('we are here');
+  let waldohere = document.querySelector('.photo-image');
+  waldohere.addEventListener('click', function() {
+    console.log('printing stutt');
+  });
+});
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,5 @@
 class PhotosController < ApplicationController
     def show
         @photo = Photo.find(params[:id])
-        @image_url = @photo.image_url
     end
 end
