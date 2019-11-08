dd6e228891c7fd9dfdc7aa42bec96b8e16cfd75e
Duy Lai <duyklai@gmail.com>
2019-11-07 23:20:06 -0600

    Add another map to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-photos = Photo.create([{ name: "Beach", image_url: "beach.jpg"}])
+photos = Photo.create([{ name: "Beach", image_url: "beach.jpg"}, { name: "Town Center", image_url: "towncenter.jpg"}])
 
-characters = Character.create([{ name: "Waldo", pos_x: 1123, pos_y: 335, photo_id: 1}])
+characters = Character.create([{ name: "Waldo", 
+                                 pos_x: 1123, 
+                                 pos_y: 335, 
+                                 photo_id: 1},
+                                 #Town Center Photo
+                               { name: "Waldo",
+                                 pos_x: 865,
+                                 pos_y: 658,
+                                 photo_id: 2}])
+# { name: "Wenda",
+# pos_x: 1,
+# pos_y: 1,
+# photo_id: 2},
+# { name: "Wizard",
+# pos_x: 2,
+# pos_y: 2,
+# photo_id: 2},
+# { name: "Odlaw",
+# pos_x: 3,
+# pos_y: 3,
+# photo_id: 2}
