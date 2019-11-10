eec48f45e3ba7d241974cbb7675c8ff0be381a4e
Duy Lai <duyklai@gmail.com>
2019-11-09 20:37:51 -0600

    Updates for heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# config/application.rb
+config.assets.initialize_on_precompile = false
 
 module WheresWaldo
   class Application < Rails::Application
